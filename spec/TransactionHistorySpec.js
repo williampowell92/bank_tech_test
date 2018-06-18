f63ebb88a31dc86a0b52329db982cda042bbe98d
William Powell <william.powell92@gmail.com>
2018-06-18 15:07:30 +0100

    A Transaction can be added to TransactionHistory

--- spec/TransactionHistorySpec.js
+++ spec/TransactionHistorySpec.js
@@ -2,14 +2,27 @@ const TransactionHistory = require('../lib/TransactionHistory');
 
 describe('TransactionHistory', () => {
   let transactionHistory;
+  let transaction;
 
   beforeEach(() => {
     transactionHistory = new TransactionHistory();
+    transaction = {
+      date: new Date(2012, 1, 1),
+      amount: 100,
+      balance: 1000,
+    };
   });
 
   describe('transactions', () => {
     it('is created with no transactions', () => {
       expect(transactionHistory.transactions).toEqual([]);
+    });
+  });
+
+  describe('addTransaction', () => {
+    it('can add a transaction to an array', () => {
+      transactionHistory.add(transaction);
+      expect(transactionHistory.transactions).toEqual([transaction]);
     });
   });
 });
