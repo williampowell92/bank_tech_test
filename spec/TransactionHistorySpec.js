16708760c14361fa078d9bd36311d2e0c2dd398e
William Powell <william.powell92@gmail.com>
2018-06-18 15:09:57 +0100

    A different transaction can be added to TransactionHistory

--- spec/TransactionHistorySpec.js
+++ spec/TransactionHistorySpec.js
@@ -3,6 +3,7 @@ const TransactionHistory = require('../lib/TransactionHistory');
 describe('TransactionHistory', () => {
   let transactionHistory;
   let transaction;
+  let differentTransaction;
 
   beforeEach(() => {
     transactionHistory = new TransactionHistory();
@@ -10,6 +11,11 @@ describe('TransactionHistory', () => {
       date: new Date(2012, 1, 1),
       amount: 100,
       balance: 1000,
+    };
+    differentTransaction = {
+      date: new Date(2018, 3, 7),
+      amount: -500,
+      balance: 3000,
     };
   });
 
@@ -20,9 +26,14 @@ describe('TransactionHistory', () => {
   });
 
   describe('addTransaction', () => {
-    it('can add a transaction to an array', () => {
+    it('can add a transaction to transactions', () => {
       transactionHistory.add(transaction);
       expect(transactionHistory.transactions).toEqual([transaction]);
+    });
+
+    it('can add a different transaction to transactions', () => {
+      transactionHistory.add(differentTransaction);
+      expect(transactionHistory.transactions).toEqual([differentTransaction]);
     });
   });
 });
