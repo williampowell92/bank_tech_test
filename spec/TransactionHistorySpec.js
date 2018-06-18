8360376c8df024576d87a5b26aca4b6588f90dbf
William Powell <william.powell92@gmail.com>
2018-06-18 18:14:16 +0100

    Tested new instances of Transactions are created with correct arguments

--- spec/TransactionHistorySpec.js
+++ spec/TransactionHistorySpec.js
@@ -2,22 +2,18 @@ const TransactionHistory = require('../lib/TransactionHistory');
 
 describe('TransactionHistory', () => {
   let transactionHistory;
-  let transaction;
-  let differentTransaction;
+  let amount;
+  let balance;
+  let date;
 
   beforeEach(() => {
-    function MockTransaction() {}
+    function MockTransaction(...args) {
+      this.arguments = args;
+    }
     transactionHistory = new TransactionHistory(MockTransaction);
-    transaction = {
-      date: new Date(2012, 1, 1),
-      amount: 100,
-      balance: 1000,
-    };
-    differentTransaction = {
-      date: new Date(2018, 3, 7),
-      amount: -500,
-      balance: 3000,
-    };
+    amount = 100;
+    balance = 1000;
+    date = new Date(2012, 1, 1);
   });
 
   describe('transactions', () => {
@@ -28,22 +24,35 @@ describe('TransactionHistory', () => {
 
   describe('addTransaction', () => {
     it('adds an object into transactions', () => {
-      transactionHistory.add(100, 1000, new Date(2012, 1, 1));
+      transactionHistory.add(amount, balance, date);
 
       expect(transactionHistory.transactions.length).toEqual(1);
     });
 
     it('creates an instance of a transaction object', () => {
-      transactionHistory.add(100, 1000, new Date(2012, 1, 1));
+      transactionHistory.add(amount, balance, date);
 
       expect(transactionHistory.transactions[0].__proto__.constructor.name).toEqual('MockTransaction');
     });
 
     it('adds multiple object into transactions', () => {
-      transactionHistory.add(100, 1000, new Date(2012, 1, 1));
-      transactionHistory.add(100, 1000, new Date(2012, 1, 1));
+      transactionHistory.add(amount, balance, date);
+      transactionHistory.add(amount, balance, date);
 
       expect(transactionHistory.transactions.length).toEqual(2);
+    });
+
+    it('creates another instance of a transaction object', () => {
+      transactionHistory.add(amount, balance, date);
+      transactionHistory.add(amount, balance, date);
+
+      expect(transactionHistory.transactions[1].__proto__.constructor.name).toEqual('MockTransaction');
+    });
+
+    it('creates an instance of transaction with correct arguments', () => {
+      transactionHistory.add(amount, balance, date);
+
+      expect(transactionHistory.transactions[0].arguments).toEqual([amount, balance, date]);
     });
   });
 });
