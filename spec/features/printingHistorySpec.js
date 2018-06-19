f271c61ce14787df5ca27fa6e5460dd94d8b65b1
William Powell <william.powell92@gmail.com>
2018-06-19 13:29:44 +0100

    Statement now prints transactions in reverse order

--- spec/features/printingHistorySpec.js
+++ spec/features/printingHistorySpec.js
@@ -1,26 +1,43 @@
+/* eslint no-global-assign: 0 */
+
 const Account = require('../../lib/Account');
 
 describe('printing balance history', () => {
   let account;
-  const oldLog = console.log;
+  const oldConsole = console;
+  let consoleLogHistory;
 
   beforeEach(() => {
-    console.log = jasmine.createSpy('log');
+    account = new Account();
+
+    consoleLogHistory = [];
+    console = {
+      log() {},
+    };
+    spyOn(console, 'log').and.callFake((...args) => {
+      consoleLogHistory.push(args);
+    });
   });
 
   afterEach(() => {
-    console.log = oldLog;
+    console = oldConsole;
+    consoleLogHistory = [];
   });
 
   it('prints correct balance history', () => {
-    account = new Account();
+    const expectedArguments = [
+      ['date || credit || debit || balance'],
+      ['14/01/2012 || || 500.00 || 2500.00'],
+      ['13/01/2012 || 2000.00 || || 3000.00'],
+      ['10/01/2012 || 1000.00 || || 1000.00'],
+    ];
     account.deposit(1000, new Date(2012, 0, 10));
     account.deposit(2000, new Date(2012, 0, 13));
     account.withdraw(500, new Date(2012, 0, 14));
     account.printStatement();
-    expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance');
-    expect(console.log).toHaveBeenCalledWith('14/01/2012 || || 500.00 || 2500.00');
-    expect(console.log).toHaveBeenCalledWith('13/01/2012 || 2000.00 || || 3000.00');
-    expect(console.log).toHaveBeenCalledWith('10/01/2012 || 1000.00 || || 1000.00');
+
+    expectedArguments.forEach((expectedArgument, index) => {
+      expect(consoleLogHistory[index]).toEqual(expectedArgument);
+    });
   });
 });
