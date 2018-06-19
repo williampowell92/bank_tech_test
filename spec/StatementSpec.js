b9d02d303fb49aaa3435e77cb317fc2e093e2240
William Powell <william.powell92@gmail.com>
2018-06-19 11:01:48 +0100

    Statement can print a different transactions

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -3,17 +3,26 @@ const Statement = require('../lib/Statement');
 describe('Statement', () => {
   let statement;
   let transactionHistory;
+  let differentTransactionHistory;
   const oldLog = console.log;
 
   beforeEach(() => {
     statement = new Statement();
 
     transactionHistory = {
-      transactions: {
+      transactions: [{
         amount: 1000,
         balance: 1000,
-        date: new Date(2012, 1, 10),
-      },
+        date: new Date(2012, 0, 10),
+      }],
+    };
+
+    differentTransactionHistory = {
+      transactions: [{
+        amount: 2000,
+        balance: 3000,
+        date: new Date(2012, 0, 13),
+      }],
     };
 
     console.log = jasmine.createSpy('log');
@@ -34,6 +43,12 @@ describe('Statement', () => {
       statement.print(transactionHistory);
 
       expect(console.log).toHaveBeenCalledWith('10/01/2012 || 1000.00 || || 1000.00');
+    });
+
+    it('should print a different transaction', () => {
+      statement.print(differentTransactionHistory);
+
+      expect(console.log).toHaveBeenCalledWith('13/01/2012 || 2000.00 || || 3000.00');
     });
   });
 });
