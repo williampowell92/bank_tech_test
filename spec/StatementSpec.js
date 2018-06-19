1d8a25a68ff4031e6161fc69e6568206f7a9fbe9
William Powell <william.powell92@gmail.com>
2018-06-19 14:12:32 +0100

    Rewrite statement specs to take into account order of console log calls

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,3 +1,5 @@
+/* eslint no-global-assign: 0 */
+
 const Statement = require('../lib/Statement');
 
 describe('Statement', () => {
@@ -7,10 +9,19 @@ describe('Statement', () => {
   let multipleTransactionHistory;
   let negativeTransactionHistory;
   let differentNegativeTransactionHistory;
-  const oldLog = console.log;
+  const oldConsole = console;
+  let consoleLogHistory;
 
   beforeEach(() => {
     statement = new Statement();
+
+    consoleLogHistory = [];
+    console = {
+      log() {},
+    };
+    spyOn(console, 'log').and.callFake((...args) => {
+      consoleLogHistory.push(args);
+    });
 
     transactionHistory = {
       transactions: [{
@@ -56,49 +67,58 @@ describe('Statement', () => {
         date: new Date(2012, 0, 13),
       }],
     };
-
-    console.log = jasmine.createSpy('log');
   });
 
   afterEach(() => {
-    console.log = oldLog;
+    console = oldConsole;
+    consoleLogHistory = [];
   });
 
   describe('print', () => {
-    it('should print a title', () => {
-      statement.print(transactionHistory);
+    describe('single transactions', () => {
+      it('should print a title', () => {
+        statement.print(transactionHistory);
 
-      expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance');
+        expect(console.log).toHaveBeenCalledWith('date || credit || debit || balance');
+      });
+
+      it('should print a transaction', () => {
+        statement.print(transactionHistory);
+
+        expect(console.log).toHaveBeenCalledWith('10/01/2012 || 1000.00 || || 1000.00');
+      });
+
+      it('should print a different transaction', () => {
+        statement.print(differentTransactionHistory);
+
+        expect(console.log).toHaveBeenCalledWith('13/01/2012 || 2000.00 || || 3000.00');
+      });
+
+      it('should print a transaction with a negative amount', () => {
+        statement.print(negativeTransactionHistory);
+
+        expect(console.log).toHaveBeenCalledWith('14/01/2012 || || 500.00 || 2500.00');
+      });
+
+      it('should print a different transaction with a negative amount', () => {
+        statement.print(differentNegativeTransactionHistory);
+
+        expect(console.log).toHaveBeenCalledWith('13/01/2012 || || 2000.00 || -3000.00');
+      });
     });
 
-    it('should print a transaction', () => {
-      statement.print(transactionHistory);
+    describe('multiple transactions', () => {
+      it('should print the first of multiple transactions', () => {
+        statement.print(multipleTransactionHistory);
 
-      expect(console.log).toHaveBeenCalledWith('10/01/2012 || 1000.00 || || 1000.00');
-    });
+        expect(consoleLogHistory[1]).toEqual(['13/01/2012 || 2000.00 || || 3000.00']);
+      });
 
-    it('should print a different transaction', () => {
-      statement.print(differentTransactionHistory);
+      it('should print the second of multiple transactions', () => {
+        statement.print(multipleTransactionHistory);
 
-      expect(console.log).toHaveBeenCalledWith('13/01/2012 || 2000.00 || || 3000.00');
-    });
-
-    it('should print the first of multiple transactions', () => {
-      statement.print(multipleTransactionHistory);
-
-      expect(console.log).toHaveBeenCalledWith('13/01/2012 || 2000.00 || || 3000.00');
-    });
-
-    it('should print a transaction with a negative amount', () => {
-      statement.print(negativeTransactionHistory);
-
-      expect(console.log).toHaveBeenCalledWith('14/01/2012 || || 500.00 || 2500.00');
-    });
-
-    it('should print a different transaction with a negative amount', () => {
-      statement.print(differentNegativeTransactionHistory);
-
-      expect(console.log).toHaveBeenCalledWith('13/01/2012 || || 2000.00 || -3000.00');
+        expect(consoleLogHistory[2]).toEqual(['10/01/2012 || 1000.00 || || 1000.00']);
+      });
     });
   });
 });
